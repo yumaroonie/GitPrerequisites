c9cc910c4fb13a5a8a0478cffdaadd9e827b0af5
Chris Headley <chrisheadley20@gmail.com>
2023-09-05 21:37:44 -0700

    Made blobify method, added a blobify test to tester

--- Blob.java
+++ Blob.java
@@ -1,4 +1,38 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class Blob
 {
-    
+    private String SHA1;
+
+    public void blobify (String inputFileName, String objectsPath) throws NoSuchAlgorithmException, IOException
+    {
+        //reads file into String myString
+        Scanner scanner = new Scanner(new File(inputFileName));
+        String myString = scanner.useDelimiter("\\A").next();
+        scanner.close();
+        //hashes file with SHA1 hash code into String called SHA1)
+        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+        crypt.reset();
+        crypt.update(myString.getBytes("UTF-8"));
+        Formatter formatter = new Formatter();
+        for (byte b : crypt.digest())
+        {
+            formatter.format("%02x", b);
+        }
+        SHA1 = formatter.toString();
+        formatter.close();
+        //writes new file into objects folder
+        FileWriter writer = new FileWriter(new File (objectsPath + "/" + SHA1));
+        PrintWriter out = new PrintWriter(writer);
+        out.println (myString);
+        writer.close ();
+        out.close ();
+    }
 }
--- GitPrereqsTester.java
+++ GitPrereqsTester.java
@@ -1,10 +1,15 @@
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 public class GitPrereqsTester
 {
-    public static void main (String [] args) throws IOException
+    public static void main (String [] args) throws IOException, NoSuchAlgorithmException
     {
         Index myIndex = new Index ();
         myIndex.initialize ("/Users/chrisheadley/Desktop/Comp Sci/GitPrerequisites");
+
+        Blob myBlob = new Blob ();
+        myBlob.blobify ("/Users/chrisheadley/Desktop/Comp Sci/GitPrerequisites/ToBeBlobbed.txt","/Users/chrisheadley/Desktop/Comp Sci/GitPrerequisites/objects");
+        System.out.println (myBlob.getSHA1String());
     }
 }
