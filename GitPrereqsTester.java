7de5de262bdd853e62bff343cd33fd005b21462f
Chris Headley <chrisheadley20@gmail.com>
2023-10-01 15:13:14 -0700

    Modified unofficial tester slightly to test Index

--- GitPrereqsTester.java
+++ GitPrereqsTester.java
@@ -3,25 +3,27 @@ public class GitPrereqsTester
 {
     public static void main (String [] args) throws Exception
     {
+
         //Commit myCommit = new Commit ("chris2", "iswaytooepic");
         
-        Tree myTree = new Tree ();
-        myTree.addDirectory ("advancedDirectory");
+        //Tree myTree = new Tree ();
+        //myTree.addDirectory ("advancedDirectory");
         
-        // //tests initialize
-        // Index myIndex = new Index ();
-        // myIndex.initialize ("/Users/ryancheng/p/Honors Topics/GitPrerequisites");
+        //tests initialize
+        Index myIndex = new Index ();
+        myIndex.initialize ("/Users/chrisheadley/Desktop/Comp Sci/GitPrerequisites");
 
         // //tests blobify, printing sha1 of file contents
         // //Blob myBlob = new Blob ("/Users/chrisheadley/Desktop/Comp Sci/GitPrerequisites/ToBeBlobbed.txt");
         // //myBlob.blobify ("/Users/chrisheadley/Desktop/Comp Sci/GitPrerequisites/objects");
         // //System.out.println (myBlob.getSHA1String());
 
-        // myIndex.add ("foo.txt");
-        // myIndex.add ("foobar.txt");
-        // myIndex.add ("bar.txt");
-        // myIndex.add ("bar.txt");
-        // myIndex.remove("foobar.txt");
+        myIndex.add ("foo.txt");
+        myIndex.add ("foobar.txt");
+        myIndex.add ("bar.txt");
+        myIndex.add ("bar.txt");
+        myIndex.remove("foobar.txt");
+        myIndex.updateIndex();
 
         // Blob blobTest = new Blob("testfile.txt", "./");
         /*
